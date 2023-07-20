6ccff56d3a48a359a6ffc3b5a6f12b4654511bc9
Moritz Schloegel <moritz.schloegel@rub.de>
2023-07-20 10:16:45 +0200

    fix case study: add missing header file and fix LLVM location to match the one within our Docker

--- loki/testcases/libdvdcss/libdvdcss/install-modified-libdvdcss.sh
+++ loki/testcases/libdvdcss/libdvdcss/install-modified-libdvdcss.sh
@@ -2,7 +2,7 @@
 
 autoreconf -i
 
-export LLVM_INSTALLED=$(pwd)/../../../instruction_handler_implementation/llvm 
+export LLVM_INSTALLED=/llvm 
 export INSTALL_DIR=$(pwd)/libdvdcss-build
 export INSTALL_DIR_MOD=$(pwd)/libdvdcss-build-modified
 
@@ -10,6 +10,8 @@ export CC=$LLVM_INSTALLED/bin/clang
 export CXX=$LLVM_INSTALLED/bin/clang++
 
 PARALLEL_JOBS=${PARALLEL_JOBS:-1}
+
+cp template.hpp $(pwd)/../../../translator/src
 
 # reverse patch (might have already been applied)
 patch -R -N ./src/css.c patch_DecryptKey.diff
---
+++ loki/testcases/libdvdcss/libdvdcss/template.hpp
@@ -0,0 +1,8 @@
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+#include <stdint.h>
+
+int vm_entry_wrapper( uint8_t invert, uint8_t *p_key, uint8_t *p_crypted, uint8_t *p_result );
+
+#endif
